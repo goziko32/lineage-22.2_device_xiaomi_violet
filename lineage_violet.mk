b7da6b369c43fdba0bebdde307ed1a0ce5f4bea0
Chronix <phoniexpaul88@gmail.com>
2025-07-10 13:45:04 +0530

    @Matrixx Bringup

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -11,17 +11,30 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common EvoX stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-#EvoX Flags
-EVO_BUILD_TYPE := Unofficial
+#Matrixx Flags
+# To include Gapps 
+WITH_GMS := true
+
+# To Build Google(Dailer, Message, Phone) and BCR
+WITH_GMS_COMMS_SUITE := true
+
+# To Add cinematic wallpaer support (only supported in gapps build not in vanilla remove this flag for vanilla builds)
+TARGET_SUPPORTS_WALLEFFECT := true
+
+#Some more GMS Flag
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_STOCK_AICORE := true
+
+# To Add Bypass Charging Support (need to be adapted in DT and KT as well)
+BYPASS_CHARGE_SUPPORTED  := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BUILD_APERTURE_CAMERA := false
-BYPASS_CHARGE_SUPPORTED  := true
 
 #Include ViperFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 #GAPPS
-WITH_GMS := true
 WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
