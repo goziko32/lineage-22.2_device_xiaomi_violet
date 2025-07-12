0233ca18969d486a8709b050934c00e4864427b0
GoZiko <anirban89m@gmail.com>
2025-07-12 08:07:44 +0530

    Update device.mk Added Viper

--- device.mk
+++ device.mk
@@ -15,6 +15,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+#Include ViperFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
